9d19e3257fe5f86484d38455a962795c1b9f18c3
Chris Loeper <chrisloeper@gmail.com>
2019-11-28 10:51:15 -0800

    updates store to use sagas

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,18 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { sendReducer } from "./Send/Send.reducer";
 import { appReducer } from "./App/App.reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
-import {} 
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const rootReducer = combineReducers({ app: appReducer, send: sendReducer });
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(
+  rootReducer,
+  {},
+  compose(applyMiddleware(sagaMiddleware), composeWithDevTools())
+);
+
+sagaMiddleware.run(rootSaga);
