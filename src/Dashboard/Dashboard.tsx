3c2cd0b6b71cdbfa39620e2e15268380e8891c31
Chris Loeper <chrisloeper@gmail.com>
2019-11-28 11:48:40 -0800

    Updates dashboard layout to match designs

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Card } from "@material-ui/core";
+
+import { Balance } from "../components/Balance/Balance";
+
+import styles from "./Dashboard.module.css";
 
 export const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.dashboardContainer}>
+      <div className={styles.widgetContainer}>
+        <Balance balance={"12.87"} />
+        <br />
+        <Card>Send</Card>
+      </div>
+      <div className={styles.graphContainer}>
+        <Card>Graph</Card>
+      </div>
+    </div>
+  );
 };
